c566e4a46f16facc30e7d1c25903b07420b5cf7a
zoufeng <casafo@163.com>
2021-04-26 21:39:40 +0800

    feat: 详情页侧边组件

---
+++ weixin-cloud-miniprogram/miniprogram/src/components/movableSidebar/index.js
@@ -0,0 +1,31 @@
+const app = getApp()
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+  lifetimes: {
+    attached() {
+      const { windowHeight } = app.globalData.systemInfo
+      this.setData({
+        moveAreaHeight: `${windowHeight - 150}px`
+      })
+    }
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    moveAreaHeight: "600px",
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
